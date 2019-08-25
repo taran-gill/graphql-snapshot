7bd0daa0e515ff5e9af8be64f2e7a830040e3d98
taran-gill <gill.tarandeep@yahoo.ca>
2019-08-25 18:15:50 -0400

    Add coverage options for Jest.

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -8,5 +8,10 @@ module.exports = {
         '\\.(gql|graphql)$': 'jest-transform-graphql',
         '^.+\\.(js|jsx)?$': 'babel-jest',
         '\\.(ts|tsx)$': 'ts-jest'
-    }
+    },
+
+    // Coverage needs to be enabled through the --coverage flag
+    collectCoverageFrom: ['src/**/*.{js,ts}'],
+    coveragePathIgnorePatterns: ['@types'],
+    coverageReporters: ['json', 'text']
 };
